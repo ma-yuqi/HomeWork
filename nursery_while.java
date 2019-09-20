5b2a8f863edaadfb5d2186040abe3959a73fb448
L <54886679+ma-yuqi@users.noreply.github.com>
2019-09-20 20:39:35 +0800

    Update nursery_while.java

--- nursery_while.java
+++ nursery_while.java
@@ -1,20 +1,17 @@
-	public class nursery_while{
+public class nursery_while {
 	public static void main(String[] args){
 		int i=99;
-			while(i>=0){
+			while(i>0){
 				if(i>2){
 					System.out.println(i+" bottles of beer on the wall,"+i+" bottles of beer\nTake one down\npass it around\n"+(i-1)+" bottles of beer on the wall\n");
 					--i;
 					}else if(i==2){
 					System.out.println(i+" bottles of beer on the wall,"+i+" bottles of beer\nTake one down\npass it around\n"+(i-1)+" bottle of beer on the wall\n");
 					--i;
-					}else if(i==1){
-					System.out.println(i+" bottle of beer on the wall,"+i+" bottle of beer\nTake one down\npass it around\n"+(i-1)+" bottle of beer on the wall\n");
-					--i;
-					}else {
-					System.out.println("No more bottles of beer on the wall");
+					}else{
+					System.out.println(i+" bottle of beer on the wall,"+i+" bottle of beer\nTake one down\npass it around\n"+(i-1)+" bottle of beer on the wall\n\nNo more bottles of beer on the wall");
 					--i;
 				}
 			}
 		}
-	}
+}
