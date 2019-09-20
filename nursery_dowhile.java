49e9cc61b8e7174e6583cf0cd78ee017f8d515a8
L <54886679+ma-yuqi@users.noreply.github.com>
2019-09-20 20:40:58 +0800

    Update nursery_dowhile.java

--- nursery_dowhile.java
+++ nursery_dowhile.java
@@ -1,4 +1,4 @@
-	public class nursery_dowhile{
+public class nursery_dowhile {
 	public static void main(String[] args){
 		int i=99;
 				do{if(i>2){
@@ -7,13 +7,10 @@
 					}else if(i==2){
 					System.out.println(i+" bottles of beer on the wall,"+i+" bottles of beer\nTake one down\npass it around\n"+(i-1)+" bottle of beer on the wall\n");
 					--i;
-					}else if(i==1){
-					System.out.println(i+" bottle of beer on the wall,"+i+" bottle of beer\nTake one down\npass it around\n"+(i-1)+" bottle of beer on the wall\n");
-					--i;
-					}else {
-					System.out.println("No more bottles of beer on the wall");
+					}else{
+					System.out.println(i+" bottle of beer on the wall,"+i+" bottle of beer\nTake one down\npass it around\n"+(i-1)+" bottle of beer on the wall\n\nNo more bottles of beer on the wall");
 					--i;
 				}
-			}while(i>=0);
+			}while(i>0);
 		}
-	}
+}
