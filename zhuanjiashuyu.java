e8df8ccd32ee42d466120517a164339920511472
L <54886679+ma-yuqi@users.noreply.github.com>
2019-09-24 21:19:44 +0800

    Update zhuanjiashuyu.java

--- zhuanjiashuyu.java
+++ zhuanjiashuyu.java
@@ -2,7 +2,7 @@ public class zhuanjiashuyu{
 	public static void main(String[] args){
 		String[] one={"123","234","345","456","567","678"};
 		String[] two={"teacher","student","doctor","master","bachelor","professor"};
-		String[] three={"school","classroom","home","playground","office","
+		String[] three={"school","classroom","home","playground","office","gym"};
 		int rand1=(int)(Math.random()*6);
 		int rand2=(int)(Math.random()*6);
 		int rand3=(int)(Math.random()*6);
